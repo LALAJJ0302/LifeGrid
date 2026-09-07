2c2759109c4a9d841f208d18c4a7bc95eda72389
JJ <curry520paul@gmail.com>
2026-09-07 12:46:41 +1000

    Initial Commit

---
+++ LifeGrid/LifeGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeGridApp.swift
+//  LifeGrid
+//
+//  Created by JJ on 7/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct LifeGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
